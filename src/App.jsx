91aa987feb68691eafc7a535dae6565b4d2c62e8
atulya-srivastava <vns.atulyasrivastava@gmail.com>
2025-10-18 17:29:07 +0530

    add route for dyanamic programming

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SortingPage from "./pages/sorting/SortingPage";
 import GraphPage from "./pages/graph/GraphPage";
 import Homepage from "./pages/Homepage.jsx";
+import DynamicProgrammingPage from "./pages/dynamic-programming/DyanmicProgrammingPage.jsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         {/* <Route path="/graph/union-find" element={<UnionFindPage />} /> */}
         <Route path="/sorting" element={<SortingPage />} />
         <Route path="/graph" element={<GraphPage />} />
+        <Route path="/dynamic-programming" element={<DynamicProgrammingPage />} />
       </Routes>
     </Router>
   );
